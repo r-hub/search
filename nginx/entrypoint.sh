c2f76ba3c027e63ce22f2d7318cee520a309dac9
Gábor Csárdi <csardi.gabor@gmail.com>
2018-11-04 02:13:14 +0000

    Make nginx wait for elasticsearch
    We wait now until the index can be queried. Then we
    should be good to go.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,4 +1,13 @@
 
 set -e
 
-wait-for elasticsearch:9200 -- nginx -g "daemon off;"
+while true
+do
+    echo "waiting for DB"
+    status=$(curl -s -o /dev/null -w "%{http_code}" \
+		  http://elasticsearch:9200/package || echo 500)
+    if [ "x$status" == "x200" ]; then break; fi
+    sleep 1
+done
+
+nginx -g "daemon off;"
